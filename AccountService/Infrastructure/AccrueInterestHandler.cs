7d6153c37fb5f3ecd9d383302e87ff9d831ac826
Kosty <kosty.lygin.58@mail.ru>
2025-08-12 03:11:04 +0300

    Подключение Hangfire

---
+++ AccountService/Infrastructure/AccrueInterestHandler.cs
@@ -0,0 +1,30 @@
+﻿using AccountService.Features.Accounts;
+using AccountService.Infrastructure.Dapper.Interfaces;
+using AccountService.Infrastructure.Dapper.Models;
+
+namespace AccountService.Infrastructure;
+
+public class AccrueInterestHandler(IDapperContext<IDapperSettings> context, IAccountRepository accountRepository)
+{
+    public async Task HandleAsync()
+    {
+        using var transaction = context.BeginTransaction();
+        try
+        {
+            var accountIds = await accountRepository.GetAllAccountIdsAsync();
+
+            foreach (var id in accountIds)
+            {
+                await transaction.Command(
+                    new QueryObject("CALL accrue_interest(@Id)", new { Id = id }));
+            }
+
+            transaction.Commit();
+        }
+        catch
+        {
+            transaction.Rollback();
+            throw;
+        }
+    }
+}
